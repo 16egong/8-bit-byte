82c4b5b16ab8154ce66cec7bca1f20791e9fd46d
Kevin Beal <tbeal@umd.edu>
2020-04-25 14:14:49 -0400

    first heroku push

--- src/components/ByteButton/ByteButton.js
+++ src/components/ByteButton/ByteButton.js
@@ -3,8 +3,14 @@ import { Link } from "react-router-dom";
 import "./button.css";
 
 const ByteButton = (props) => {
+  let { backgroundColor, textColor } = props;
   return (
-    <Link to={props.to} className="byte-button" onClick={() => props.onClick()}>
+    <Link
+      to={props.to}
+      className="byte-button"
+      style={{ backgroundColor: backgroundColor, color: textColor }}
+      onClick={() => props.onClick()}
+    >
       {props.label}
     </Link>
   );
--- src/components/Page/index.js
+++ src/components/Page/index.js
@@ -1,17 +1,65 @@
 import React from "react";
 import Page from "./Page";
 import Home from "./HomePage";
+import ByteButton from "../ByteButton";
+import { Link } from "react-router-dom";
 
 const HomePage = () => {
   return <Home />;
 };
 
 const LoginPage = () => {
-  return <Page title="Login" subtitle="Login to your account" />;
+  return (
+    <Page
+      title="Login"
+      subtitle="Login to your account"
+      backgroundColor="#f2ecec"
+    >
+      <div className="page-content-container">
+        <ByteButton
+          label="Login"
+          backgroundColor="#efc326"
+          textColor="white"
+          onClick={() => alert("Goes to World Map")}
+        />
+        <p>
+          Don't have an account?{" "}
+          <Link to="/register" style={{ color: "#efc326" }}>
+            Sign Up!
+          </Link>
+        </p>
+        <p>
+          Forgot your password/username?{" "}
+          <Link style={{ color: "#efc326" }}>Click Here!</Link>
+        </p>
+      </div>
+    </Page>
+  );
 };
 
 const RegisterPage = () => {
-  return <Page title="Register" subtitle="Register a new acccount" />;
+  return (
+    <Page
+      title="Register"
+      subtitle="Register a new acccount"
+      backgroundColor="#f2ecec"
+    >
+      <div className="page-content-container">
+        <ByteButton
+          label="Register"
+          backgroundColor="#efc326"
+          textColor="white"
+          onClick={() => alert("Goes to Dietary Preferences")}
+        />
+        <p>
+          Already have an account?{" "}
+          <Link to="/login" style={{ color: "#efc326" }}>
+            Login!
+          </Link>
+        </p>
+      </div>
+    </Page>
+  );
 };
 
 export { HomePage, LoginPage, RegisterPage };
